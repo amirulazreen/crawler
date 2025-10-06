9c35115434ed263f4112166226c4f05e55336ece
Amirul Azreen <amirulazreen1994@gmail.com>
2025-10-06 08:52:26 +0800

    organized v2

--- libraries/colly/colly.go
+++ libraries/colly/colly.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strings"
@@ -24,27 +25,41 @@ func CrawlWebsite(website string) []models.Page {
 	}
 	host := u.Hostname()
 
+	userAgents := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0",
+	}
+
 	c := colly.NewCollector(
 		colly.AllowedDomains(host),
-		colly.MaxDepth(0),
+		colly.MaxDepth(2),
+		colly.UserAgent(userAgents[rand.Intn(len(userAgents))]),
 	)
 
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*." + host,
-		Parallelism: 5,
-		Delay:       1 * time.Second,
-		RandomDelay: 1 * time.Second,
+		Parallelism: 10,
+		Delay:       200 * time.Millisecond,
+		RandomDelay: 300 * time.Millisecond,
 	})
 
+	// Optimized HTTP transport for better performance
 	c.WithTransport(&http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: false,
 		},
-		IdleConnTimeout:   10 * time.Second,
-		DisableKeepAlives: false,
+		IdleConnTimeout:       30 * time.Second,
+		DisableKeepAlives:     false,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   10,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	})
 
-	c.SetRequestTimeout(15 * time.Second)
+	c.SetRequestTimeout(10 * time.Second)
 
 	c.OnHTML("title", func(e *colly.HTMLElement) {
 		url := e.Request.URL.String()
@@ -76,9 +91,29 @@ func CrawlWebsite(website string) []models.Page {
 		}
 	})
 
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		r.Headers.Set("Accept-Language", "en-US,en;q=0.5")
+		r.Headers.Set("Accept-Encoding", "gzip, deflate")
+		r.Headers.Set("DNT", "1")
+		r.Headers.Set("Connection", "keep-alive")
+		r.Headers.Set("Upgrade-Insecure-Requests", "1")
+	})
+
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Request.AbsoluteURL(e.Attr("href"))
 		if link == "" {
+			return
+		}
+
+		if strings.HasPrefix(link, "mailto:") ||
+			strings.HasPrefix(link, "tel:") ||
+			strings.HasPrefix(link, "javascript:") ||
+			strings.Contains(link, "#") ||
+			strings.HasSuffix(link, ".pdf") ||
+			strings.HasSuffix(link, ".jpg") ||
+			strings.HasSuffix(link, ".png") ||
+			strings.HasSuffix(link, ".gif") {
 			return
 		}
 
@@ -101,6 +136,16 @@ func CrawlWebsite(website string) []models.Page {
 
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Error:", r.Request.URL, err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Printf("Visited: %s (Status: %d)\n", r.Request.URL, r.StatusCode)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		if len(results) > 1000 {
+			r.Abort()
+		}
 	})
 
 	c.Visit(website)
